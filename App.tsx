e431a2f804f4639fb01a49253c3c9cf45d33e56b
vance-denson <vadeson17@protonmail.com>
2021-10-27 23:50:32 -0400

    setup

--- App.tsx
+++ App.tsx
@@ -1,21 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+import MainLayout from './layouts/MainLayout';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <MainLayout>
+      <Text style={styles.splashButtons}>Listen to the Podcast</Text>
+      <Text style={[styles.splashButtons, { marginTop: '4rem' }]}>
+        About Us
+      </Text>
       <StatusBar style="auto" />
-    </View>
+    </MainLayout>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  splashButtons: {
+    height: '40%',
+    width: '80%',
+    backgroundColor: '#FC2',
+    borderRadius: 50,
+    alignContent: 'center',
     justifyContent: 'center',
+    display: 'grid',
+    fontSize: '2rem',
+    // flex: 1,
   },
 });
